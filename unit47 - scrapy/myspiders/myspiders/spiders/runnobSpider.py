a3f847a391e64362d5b656fb18d2cc39594b3eff
success <849962874@qq.com>
2018-12-19 17:27:05 +0800

    update runnobSpider.py

--- unit47 - scrapy/myspiders/myspiders/spiders/runnobSpider.py
+++ unit47 - scrapy/myspiders/myspiders/spiders/runnobSpider.py
@@ -5,7 +5,8 @@ import scrapy
 class RunnobspiderSpider(scrapy.Spider):
     name = 'runnobSpider'
     allowed_domains = ['http://www.runoob.com']
-    start_urls = ['http://http://www.runoob.com/']
+    start_urls = ['http://www.runoob.com/']
 
     def parse(self, response):
+        print(response.body)
         pass
